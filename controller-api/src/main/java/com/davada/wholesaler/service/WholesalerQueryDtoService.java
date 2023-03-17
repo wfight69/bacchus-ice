707951c68817bd82dc68cfd8fa65e71007056064
wfight69 <wfight69.gmail.com>
2023-03-18 03:01:35 +0900

    fixed: wholesalerDto NativeQuery테스트 session

--- controller-api/src/main/java/com/davada/wholesaler/service/WholesalerQueryDtoService.java
+++ controller-api/src/main/java/com/davada/wholesaler/service/WholesalerQueryDtoService.java
@@ -1,6 +1,7 @@
 package com.davada.wholesaler.service;
 
 import com.davada.dto.wholesaler.WholesalerDto;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
@@ -17,7 +18,7 @@ public class WholesalerQueryDtoService {
 
     public List<WholesalerDto> retrieveAllDtoWholesaler(String wholesalerCode, String wholesalerName, int limit, int offset) {
         //
-        String query =  "SELECT b.icesaler_code as icesalerCode, " +
+        String sql =    "SELECT b.icesaler_code as icesalerCode, " +
                         "       b.icesaler_name as icesalerName, " +
                         "       a.wholesaler_code as wholesalerCode, " +
                         "       a.wholesaler_name as wholesalerName, " +
@@ -27,22 +28,56 @@ public class WholesalerQueryDtoService {
                         "AND    a.wholesaler_code LIKE :wholesaler_code ";
                         //
                         if (!wholesalerName.isEmpty()) {
-                            query += "AND    a.wholesaler_name LIKE :wholesaler_name ";
+                            sql += "AND    a.wholesaler_name LIKE :wholesaler_name ";
                         }
-                        query += "LIMIT  :limit, :offset  ";
+                        sql += "LIMIT  :limit, :offset  ";
         //
-        Query nativeQuery = entityManager.createNativeQuery(query);
-        nativeQuery.setParameter("limit", limit);
-        nativeQuery.setParameter("offset", offset);
-        nativeQuery.setParameter("wholesaler_code", '%' + wholesalerCode + '%');
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("limit", limit);
+        query.setParameter("offset", offset);
+        query.setParameter("wholesaler_code", '%' + wholesalerCode + '%');
         //
         if (!wholesalerName.trim().isEmpty()) {
-            nativeQuery.setParameter("wholesaler_name", '%' + wholesalerName.trim() + '%');
+            query.setParameter("wholesaler_name", '%' + wholesalerName.trim() + '%');
         }
         //
-        // 결과처러
-        List<WholesalerDto> wholesalers = nativeQuery.getResultList();
+        // 결과처러 : entityManager is getResultList
+        List<WholesalerDto> wholesalers = query.getResultList();
 
+        return wholesalers;
+    }
+
+    public List<WholesalerDto> retrieveAllDtoWholesaler_session(String wholesalerCode, String wholesalerName, int limit, int offset) {
+        //
+        Session session = entityManager.unwrap(Session.class);
+
+        String sql =  "SELECT b.icesaler_code as icesalerCode, " +
+                "       b.icesaler_name as icesalerName, " +
+                "       a.wholesaler_code as wholesalerCode, " +
+                "       a.wholesaler_name as wholesalerName, " +
+                "       a.business_number as businessNumber " +
+                "FROM   wholesaler a, icesaler b " +
+                "WHERE  a.icesaler_uuid = b.icesaler_uuid " +
+                "AND    a.wholesaler_code LIKE :wholesaler_code ";
+        //
+        if (!wholesalerName.isEmpty()) {
+            sql += "AND    a.wholesaler_name LIKE :wholesaler_name ";
+        }
+        sql += "LIMIT  :limit, :offset  ";
+        //
+        SQLQuery query = session.createSQLQuery(sql);
+        query.setParameter("limit", limit);
+        query.setParameter("offset", offset);
+        query.setParameter("wholesaler_code", '%' + wholesalerCode + '%');
+        //
+        if (!wholesalerName.trim().isEmpty()) {
+            query.setParameter("wholesaler_name", '%' + wholesalerName.trim() + '%');
+        }
+        //
+        // 결과처러 : entityManager is getResultList
+        //         session is list
+        List<WholesalerDto> wholesalers = query.list();
+        //
         return wholesalers;
     }
 }
--- domain/src/main/java/com/davada/dto/wholesaler/WholesalerDto.java
+++ domain/src/main/java/com/davada/dto/wholesaler/WholesalerDto.java
@@ -5,7 +5,7 @@ import lombok.*;
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class WholesalerDto {
     //
     String icesalerCode;
